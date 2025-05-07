f9a537691ba4e1408927fe1f0f10389d5e694414
Rana <reduanulislam92665@gmail.com>
2025-05-07 19:20:35 +0600

    p-1

--- assigment/assigment1.ts
+++ assigment/assigment1.ts
@@ -1,1 +1,21 @@
-console.log("Hello world")
+//
+function formatString (input : string , flag ? : boolean) : string {
+      if (flag == true || flag == null){
+            return input.toUpperCase();
+      }
+      if (flag == false) {
+            return input.toLowerCase();
+      }
+      else {
+            return `${input} is not a string`
+      }
+}
+
+const example1 = formatString("Hello");
+const example2 = formatString("Hello", false);
+const example3 = formatString("Hello", true);
+
+console.log(example1)
+console.log(example2)
+console.log(example3)
+//
